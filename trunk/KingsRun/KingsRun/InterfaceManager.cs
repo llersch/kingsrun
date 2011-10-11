4731eb0d818fbb5e05eb628592c7962febcd0e7b
lucaslersch <lucaslersch@ec9a35fd-c71d-d898-cfba-9ed23fa82ae9>
2011-10-11 22:24:04 +0000

    - Adicionado o InterfaceManager

---
+++ trunk/KingsRun/KingsRun/InterfaceManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KingsRun
+{
+    class InterfaceManager
+    {
+        HexGrid grid = new HexGrid(47);
+
+        public InterfaceManager(int _radius, int _xoff, int _yoff)
+        {
+
+
+        }
+
+    }
+}
