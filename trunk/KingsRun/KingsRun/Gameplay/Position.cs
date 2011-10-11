913e5c7f91413ba701b2ea06ed57d7062f971942
vaulttech <vaulttech@ec9a35fd-c71d-d898-cfba-9ed23fa82ae9>
2011-10-11 00:31:09 +0000

    Created the Gameplay part of the game. Nothing working as it should really be D=

---
+++ trunk/KingsRun/KingsRun/Gameplay/Position.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KingsRun.Gameplay
+{
+    class Position
+    {
+        public Tuple<int, int> position;
+
+        public Position()
+            : this(0,0)
+        { }
+
+        public Position(int initColumn, int initRow)
+        {
+            column = initColumn;
+            row = initRow;
+        }
+    }
+}
