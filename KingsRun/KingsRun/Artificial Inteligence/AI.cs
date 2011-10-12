9ac6473e4e82ce7403c4432a07e0a92f35465743
lucaslersch@gmail.com <lucaslersch@gmail.com>
2011-10-12 03:12:18 +0000

    - Fazendo classe InterfaceManager

--- KingsRun/KingsRun/Artificial Inteligence/AI.cs
+++ KingsRun/KingsRun/Artificial Inteligence/AI.cs
@@ -9,36 +9,26 @@ namespace KingsRun
     {
         #region Const Fields
 
-
         const int MINVALUE = -10000;
-
 
         #endregion
 
         #region Private Fields
-
-
 
         private Piece bestPiece; //Melhor peca a ser movimentada
         private Position bestMove; // Melhor movimento a ser realizado pela peca acima
         private int deep = 5; // Profundidade de avaliacao
         private BoardManager board; // Tabuleiro do jogo
 
-
-
         #endregion
 
         #region Public Methods
-        
-        
         
         public AI(BoardManager aBoard)
         //construtor da classe
         {
             this.board = aBoard;
         }
-
-
 
         public void play()
         //realiza a jogada da IA
@@ -51,13 +41,9 @@ namespace KingsRun
             this.board.MoveAndKill(this.bestPiece, this.bestMove); //Realiza o movimento
         }
 
-
-
         #endregion
 
         #region Private Methods
-
-
 
         private int next(List<Piece> myPieces, List<Piece> opPieces)
         //retorna a melhor pontuacao possivel com o tabuleiro apresentado
@@ -110,7 +96,6 @@ namespace KingsRun
 
         }
 
-
         private int evaluate(List<Piece> myPieces, List<Piece> opPieces)
         //Para cada peca perdida, perde 3 pontos;
         //Para cada peca capturada, ganha 2 pontos;
@@ -131,8 +116,6 @@ namespace KingsRun
 
             return (score);
         }
-
-
 
         #endregion
     }
--- KingsRun/KingsRun/InterfaceManager.cs
+++ KingsRun/KingsRun/InterfaceManager.cs
@@ -8,12 +8,22 @@ namespace KingsRun
     class InterfaceManager
     {
         HexGrid grid;
-        BoardManager bmanager;
+        BoardManager bManager;
         
         public InterfaceManager(int _radius, int _xoff, int _yoff, BoardManager _bmanager)
         {
-            grid = new HexGrid(_radius);
-            bmanager = _bmanager;
+            grid = new HexGrid(_radius, _xoff, _yoff);
+            bManager = _bmanager;
+        }
+
+        public Position ScreenToBoard(int x, int y)
+        {
+            grid.SetCellByPoint(x, y);
+            return new Position(grid.Column, grid.Row);
+        }
+
+        public void DrawBoard(List<Piece> p1, List<Piece> p2)
+        {
         }
 
     }
